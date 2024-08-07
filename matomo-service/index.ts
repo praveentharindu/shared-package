71219a2060d7aa03102479295346eea7f5c48b79
Praveen Tharindu <praveentharindu@Praveens-MacBook-Air-2.local>
2024-08-07 10:05:30 +0530

    Added a custom metric context

--- matomo-service/index.ts
+++
@@ -1,5 +0,0 @@
-// index.js
-import { MatomoProvider, useMatomo } from './src/react/hook/matomoTrack/MatomoContext'
-import { setMatomoConfig, setMatomoTracking } from './src/non-react/matomoTrack'
-
-export { MatomoProvider, useMatomo, setMatomoConfig, setMatomoTracking };
--- matomo-service/lib/non-react/customMetric/CustomMetric.d.ts
+++
@@ -1,9 +0,0 @@
-import './global';
-/**
- * set custom dimension
- * @param customDimensioncustomDimensions
- */
-declare function setCustomDimension(customDimension: any): void;
-declare const setCustomMetricTracking: (info: any) => void;
-declare const setMetricConfig: (config: any) => void;
-export { setMetricConfig, setCustomDimension, setCustomMetricTracking };
